3ce8ca0bf5b077189a2a67ba1e24c339fe25db28
Unknown <aemam383@gmail.com>
2019-05-13 03:33:47 +0400

    list proplem

--- list_problem/list.py
+++ list_problem/list.py
@@ -15,31 +15,29 @@ Iterate through each command in order and perform the corresponding operation on
 #Output Format
 #For each command of type print, print the list on a new line.
 
-if __name__ == '__main__':
-    N = int(input())
-    l = []
-    for command in range(N):
-        x = input().split(" ")
-        command = x[0]
-        if command == "append":
-            l.append(int(x[1]))
-
-        elif command == "insert":
-            l.insert(int(x[1]),int(x[2]))
-            
-        elif command == "pop":
-            l.pop()
-
-        elif command == "sort":
-            l.sort()    
-            
-        elif command == "remove":
-            l.remove(int(x[1]))
-            
-        elif command == "reverse":
-            l = l[::-1]
-        
-        elif command == "print":
-            print(l)
-            
+nested_list = [['Harsh', 20], ['Beria', 20], ['Varun', 19], ['Kakunami', 19], ['Vikas', 21]]
+score_list = []
+for i in nested_list:
+    score_list.append(i[1])
+print(score_list)
+mini = min(score_list)
+print(mini)
+score_list = sorted(score_list)
+nun_mini = score_list.count(mini)
+print(nun_mini)
+score_list = score_list[nun_mini:]
+print(score_list)  
+    
+print(score_list)        
+second_lowest = min(score_list)
+print(second_lowest)
+second_lowest_list = []
+for i in range(len(nested_list)):
+    if nested_list[i][1] == second_lowest:
+        second_lowest_list.append(nested_list[i][0])
+print("second lowest list: " ,second_lowest_list)
+sorted_list = sorted(second_lowest_list)
+print(sorted_list)
+for name in sorted_list:
+    print(name)
 
